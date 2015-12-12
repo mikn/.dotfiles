1fea652dba6d69adb93d84fc023eec0c1c188226
Mikael Knutsson <mikael.knutsson@king.com>
2015-12-12 10:41:28 +0000

    Adding gnupg to installation

--- debian/install.sh
+++ debian/install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Installing essentials
-apt-get install i3-wm i3lock suckless-tools wicd-curses xdm htop kpcli ranger w3m vim-nox feh aptitude xinit apt-file
+apt-get install i3-wm i3lock suckless-tools wicd-curses xdm htop kpcli ranger w3m vim-nox feh aptitude xinit apt-file gnupg
 # Installing dependencies for i3pystatus
 apt-get install python3-pip python3-netifaces libiw-dev pulseaudio libpulse0
 pip3 install basiciw libpulseaudio
