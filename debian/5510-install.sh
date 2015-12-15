82d1cd8ed83ea72f117850f4554a9690b4eef59c
Mikael Knutsson <mikael.knutsson@king.com>
2015-12-15 09:11:07 +0100

    Use rofi instead of dmenu... for now

--- debian/5510-install.sh
+++ debian/5510-install.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-set -e
-SOURCES << EOF
+read -r -d '' SOURCES <<- EOF
 deb http://httpredir.debian.org/debian/ testing main contrib non-free
 deb-src http://httpredir.debian.org/debian/ testing main contrib non-free
 
@@ -13,20 +12,21 @@ deb-src http://httpredir.debian.org/debian/ unstable main contrib non-free
 deb http://httpredir.debian.org/debian/ experimental main contrib non-free
 deb-src http://httpredir.debian.org/debian/ experimental main contrib non-free
 EOF
-UNSTABLE_PREF << EOF
+read -r -d '' UNSTABLE_PREF <<- EOF
 Package: *
 Pin: release a=unstable
 Pin-Priority: 450
 EOF
-NVIDIA_PREF << EOF
+read -r -d '' NVIDIA_PREF <<- EOF
 Package: /nvidia/
 Pin: release a=experimental
 Pin-Priority: 550
 EOF
-echo $SOURCES > /etc/apt/sources.list
-echo $UNSTABLE_PREF > /etc/apt/preferences.d/010-unstable
-echo $NVIDIA_PREF > /etc/apt/preferences.d/015-nvidia
+echo "$SOURCES" > /etc/apt/sources.list
+echo "$UNSTABLE_PREF" > /etc/apt/preferences.d/010-unstable
+echo "$NVIDIA_PREF" > /etc/apt/preferences.d/015-nvidia
 apt-get update
 apt-get install bumblebee-nvidia i965-va-driver firmware-iwlwifi xserver-xorg-video-intel xserver-xorg-video-nvidia xbacklight mesa-utils
 apt-get install tlp --no-install-recommends
-sed -i /etc/default/grub 's/"quiet"/"quiet 915.preliminary_hw_support=1 pcie_aspm=force i915.enable_rc6=7"/'
+sed -i 's/"quiet"/"quiet 915.preliminary_hw_support=1 i915.enable_rc6=3"/' /etc/default/grub
+update-grub2
--- debian/install.sh
+++ debian/install.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 # Installing essentials
-apt-get install i3-wm i3lock suckless-tools wicd-curses xdm htop kpcli ranger w3m vim-nox feh aptitude xinit apt-file gnupg
+apt-get install i3-wm i3lock suckless-tools wicd-curses xdm htop kpcli ranger w3m vim-nox feh aptitude xinit apt-file gnupg rofi
 # Installing dependencies for i3pystatus
 apt-get install python3-pip python3-netifaces libiw-dev pulseaudio libpulse0
-pip3 install basiciw libpulseaudio
+pip3 install basiciw libpulseaudio colour
 # Installing vimrc dependencies
 apt-get install silversearcher-ag
 # screen script dependencies
